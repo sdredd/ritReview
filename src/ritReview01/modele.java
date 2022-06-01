c6e0b6290e3b22394343f6b2ce557b3198b6c6be
sdredd <sdredd@gmail.com>
2022-05-31 20:37:04 -0500

    1st commit

---
+++ src/ritReview01/modele.java
@@ -0,0 +1,15 @@
+package ritReview01;
+
+public class modele {
+
+	public static void main(String[] args) {
+		String[] names = { "Natalia", "Igor", "Natalia", "Oleg", "Oleg", "Alexia", "Igor" };
+		String[] duplName = new String[names.length]; // create array where we will store duplicate names
+		int index = 1; // variable index - indicates which index to take in the array "names"
+		int indDupl = 0; // duplicate name index variable - indicates which index to take in the array
+							// "duplName"
+
+
+	}
+
+}
